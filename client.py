eb3534d7ac2d590ae2e5490f627ddefb5d10c849
DevInfinix <devinfinix@gmail.com>
2024-02-13 13:43:59 +0530

    websocket improve

--- client.py
+++ client.py
@@ -15,20 +15,39 @@ async def send_message(socket):
         await socket.send(json.dumps(data))
             
             
-async def receive_message(socket):
-    print('okkkkkkkkkkkkkk')
+async def receive_message(socket, role):
     async for message in socket:
         event = json.loads(message)
         if event['type'] == 'error':
             if event['errortype'] == 'SessionNotFound':
-                print(event['message'])       
+                print(event['message'])    
+                break
+            if event['errortype'] == 'RoomFull':
+                print(event['message'])
+                break
         if event['type'] == 'started':
             print(f"Your code is: [{event['code']}]")
         if event['type'] == 'joined':
             print(f"You joined {event['code']}")
         if event['type'] == 'message':
             print(event['message'])
-        
+        if event['type'] == 'disconnected':
+            if event['from'] == 'server':
+                if event['role'] == 'host':
+                    print('The host has been disconnected')
+                    if role == 'host':
+                        return
+                    else:
+                        break
+                elif event['role'] == 'member':
+                    print('Member disconnected')
+                    if role == 'member':
+                        return
+                    else:
+                        pass
+                    
+                
+                
 async def start(server_address, username):
     async with websockets.connect(server_address) as socket:
         event = {
@@ -36,13 +55,18 @@ async def start(server_address, username):
             'type': 'start',
             'username': username
         }
-        await socket.send(json.dumps(event))  # Send username
+        await socket.send(json.dumps(event)) 
         
-        # async def receive_message():
-        #     async for message in socket:
-        #         print(message)
-
-        await asyncio.gather(send_message(socket), receive_message(socket))
+        send_task = asyncio.create_task(send_message(socket))
+        recieve_task = asyncio.create_task(receive_message(socket, 'host'))
+        done, pending = await asyncio.wait(
+            [send_task, recieve_task],
+            return_when=asyncio.FIRST_COMPLETED,
+        )
+        for task in pending:
+            task.cancel()
+            
+        #await asyncio.gather(send_message(socket), receive_message(socket, 'host'))
         
 async def join(server_address, code, username):
     async with websockets.connect(server_address) as socket:
@@ -57,8 +81,10 @@ async def join(server_address, code, username):
         # async def receive_message():
         #     async for message in socket:
         #         print(message)
+        
+        
 
-        await asyncio.gather(receive_message(socket))
+        await asyncio.gather(receive_message(socket, 'member'))
 
 if __name__ == '__main__':
     server_address = 'ws://localhost:8080'
--- webserver.py
+++ webserver.py
@@ -3,26 +3,29 @@ import websockets
 import json
 #from async_files import FileIO
 import secrets
+import signal
+
 
 HOST = '0.0.0.0'
 PORT = 8080
 
-USERS = {}
+ROOMS = {}
 
-d = {
-    'type': 'message',
-    'from': 'client',
-    'code': 'test',
-    'role': 'host',
-    'username': 'testuser',
-    'message': 'testmessage'
-}
+# d = {
+#     'type': 'message',
+#     'from': 'client',
+#     'code': 'test',
+#     'role': 'host',
+#     'username': 'testuser',
+#     'message': 'testmessage'
+# }
 
-ERRORS = [
-    'CodeExists',
-    'CodeNotFound',
-    'ConnectionClosed',
-]
+# ERRORS = [
+#     'SessionNotFound',
+#     'RoomFull'
+# ]
+
+# EVENTS = ['start', 'started', 'join', 'joined', 'message', 'disconnected']
 
 async def error(socket, errortype, msg):
     event = {
@@ -32,13 +35,11 @@ async def error(socket, errortype, msg):
         'message': msg
     }
     await socket.send(json.dumps(event))
-
-EVENTS = ['start', 'started', 'join', 'joined', 'message']
-
+    
 async def start(socket, host):
     join_code = secrets.token_urlsafe(12)
-    connected = {socket}
-    USERS[join_code] = connected
+    ROOMS[join_code] = {'host': socket}
+    
     try:
         event = {
             'type': 'started',
@@ -47,27 +48,46 @@ async def start(socket, host):
         }
         await socket.send(json.dumps(event))
         print(f'[{join_code}] Host ({host}) started')
+        
         async for message in socket:
             message = json.loads(message)
+            
             if message['type'] == 'message':
                 print(f"[{join_code}] Host sent: {message['message']}")
-                for USER in USERS[join_code]:
-                    forward_event = {
-                        'type': 'message',
-                        'from': 'server',
-                        'message': f"[{join_code}] | [{host}]: {message['message']}"
-                    }
-                    await USER.send(json.dumps(forward_event))
+                forward_event = {
+                    'type': 'message',
+                    'from': 'server',
+                    'message': f"[{join_code}] | [{host}]: {message['message']}"
+                }
+                websockets.broadcast(list(ROOMS[join_code].values()), json.dumps(forward_event))
+                
     finally:
-        print(connected)
-        del USERS[join_code]
+        event = {
+            'type': 'disconnected',
+            'from': 'server',
+            'code': join_code,
+            'user': host,
+            'role': 'host'
+        }
+        websockets.broadcast(list(ROOMS[join_code].values()), json.dumps(event))
+        print(f"[{join_code}] | Host disconnected")
+        del ROOMS[join_code]
+    
+    
     
 async def join(socket, join_code, member):
     try:
-        connected = USERS[join_code]
+        ROOM = ROOMS[join_code]
     except:
         await error(socket, 'SessionNotFound', 'Session not found.')
-    connected.add(socket)
+        return
+        
+    if len(list(ROOM.values())) >= 2:
+        await error(socket, 'RoomFull', 'The room is full.')
+        return
+        
+    ROOM['member'] = socket
+    
     try:
         event = {
             'type': 'joined',
@@ -76,16 +96,28 @@ async def join(socket, join_code, member):
         }
         await socket.send(json.dumps(event))
         print(f'[{join_code}] Member ({member}) joined')
+        
         async for message in socket:
             message = json.loads(message)
             if message['type'] == 'message':
                 print(f"[{join_code}] Member sent: {message['message']}")
-                print(USERS[join_code])
-                async for USER in USERS[join_code]:
-                    await USER.send(f"[{join_code}] | [{member}]: {message['message']}")
+                forward_event = {
+                    'type': 'message',
+                    'from': 'server',
+                    'message': f"[{join_code}] | [{member}]: {message['message']}"
+                }
+                websockets.broadcast(list(ROOM.values()), json.dumps(forward_event))
     finally:
-        connected.remove(socket)
-        print(connected)
+        event = {
+            'type': 'disconnected',
+            'from': 'server',
+            'code': join_code,
+            'user': member,
+            'role': 'member'
+        }
+        websockets.broadcast(list(ROOM.values()), json.dumps(event))
+        print(f"[{join_code}] | Member disconnected")
+        del ROOM['member']
         
         
 async def handler(socket):
@@ -99,31 +131,31 @@ async def handler(socket):
             if event['type'] == 'join':
                 await join(socket, event['code'], user)
     except:
-        print(USERS)
-        print(f"{socket} disconnected")
-        
-    # async for message in socket:
-    #     event = json.loads(message)
-    #     fr = event['from']
-        
-    #     if fr == 'client':
-    #         eventtype = event['type']
-    #         code = event['code']
-    #         role = event['role']
-    #         uname = event['username']
-    #         msg = event['message']
-
-    #         if eventtype == 'start':
-    #             result = await start(socket, code, role, uname, msg)
-    #             if not result:
-    #                 continue
-                
+        for ROOM in ROOMS:
+            for key, value in ROOMS[ROOM].items():
+                if value == socket:
+                    event = {
+                        'type': 'disconnected',
+                        'from': 'server',
+                        'code': ROOM,
+                        'role': key
+                    }
+                    websockets.broadcast(list(ROOMS[ROOM].values()), json.dumps(event))
+                    print(f"[{ROOM}] {key} disconnected")
+                    if key == 'host':
+                        del ROOMS[ROOM]
+                    else:
+                        del ROOMS[ROOM]['member']
         
 
 async def main():
-    async with websockets.serve(handler, HOST, PORT):
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+    
+    async with websockets.serve(handler, HOST, PORT, ping_interval=5, ping_timeout=10):
         print('Server started on {HOST}:{PORT}')
-        await asyncio.Future()
+        await stop
 
 if __name__=="__main__":
     asyncio.run(main())
